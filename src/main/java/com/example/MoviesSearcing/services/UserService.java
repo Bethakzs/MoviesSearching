d0cb006f0fc641d4bfded1139b86ba94c146e010
Flashb4ekkk <145256920+Flashb4ekkk@users.noreply.github.com>
2023-12-05 18:41:59 +0200

    Revert "Try make rest controller"

--- src/main/java/com/example/MoviesSearcing/services/UserService.java
+++ src/main/java/com/example/MoviesSearcing/services/UserService.java
@@ -32,4 +32,11 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
+//    public User getFavouritesMovies(String email) {
+//        return userRepository.findByEmail(email);
+//    }
+
+//    public User saveFavouriteMovie(String email, String title) {
+//        return userRepository.updateUserByEmail(email, title);
+//    }
 }
